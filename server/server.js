374a400852d24b647c7ad674fe6a98eb3dcb9c1b
tao118 <2014130003@email.szu.edu.cn>
2019-04-09 10:43:51 +0800

    update

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 // 在本地启动服务是为了验证electron-updater的功能
+// tip: 将每次打包好的build文件夹放在server目录下
 app.listen('5000', 'localhost', () => {
     console.log('local server start at port 5000');
 });
